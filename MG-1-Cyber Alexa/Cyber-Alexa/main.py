b6958137b92b286d4cbb24c23e9fa89cc50c9f27
Cyber-Uday <udaypayghon@gmail.com>
2024-12-01 05:39:03 +0530

    Cyber alexa code

--- MG-1-Cyber Alexa/Cyber-Alexa/main.py
+++ MG-1-Cyber Alexa/Cyber-Alexa/main.py
@@ -104,7 +104,5 @@ if __name__ == "__main__":
                     audio = r.listen(source)
                     command = r.recognize_google(audio)
                     processCommand(command)
-
-
         except Exception as e:
             print("Error; {0}".format(e))
